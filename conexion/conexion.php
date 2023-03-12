e23a19a81813ec31cad586da35ae3c0a2efe1ffd
uranmarcos <marcos_uran@hotmail.com>
2023-03-12 14:44:21 -0300

    guardo pedido en base

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -3,7 +3,7 @@ class ApptivaDB {
     private $host = "localhost";
     private $usuario = "root";
     private $clave = "";
-    private $db = "vue";
+    private $db = "pedidossiPueden";
     public $conexion;
     
     public function __construct(){
@@ -13,11 +13,6 @@ class ApptivaDB {
     }
 
     public function insertar($tabla, $datos) {
-        // $resultado = $this->conexion->query("INSERT INTO $tabla VALUES(null, $datos)") or die($this->conexion->error);
-        // if ($resultado) {
-        //     return true;
-        // }
-        // return false;
         try {
             $resultado = $this->conexion->query("INSERT INTO $tabla VALUES(null, $datos)") or die();
             return true;
@@ -26,107 +21,6 @@ class ApptivaDB {
         }
     }
 
-    public function consultar($tabla, $condicion) {
-        // $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion") or die($this->conexion->error);
-        // if ($resultado) 
-        //     return $resultado->fetch_all(MYSQLI_ASSOC);
-        // return false;
-        try {
-            $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion") or die();
-            return $resultado->fetch_all(MYSQLI_ASSOC);
-        } catch (\Throwable $th) {
-            return false;
-        }
-    }
-
-    public function consultarPedidos($tabla, $condicion) {
-        try {
-            $resultado = $this->conexion->query("SELECT P.pedido, P.fecha, P.enviado,
-            CONCAT(S.localidad, ' - ', S.provincia) sede,
-            CONCAT(U.nombre, ' ', U.segundoNombre, ' ', U.apellido, ' ', U.segundoApellido) usuario
-            FROM pedidos P 
-            LEFT JOIN sedes S ON P.sede = S.id
-            LEFT JOIN usuarios U ON P.usuario = U.id
-            WHERE $condicion
-            ORDER BY fecha DESC") or die();
-            return $resultado->fetch_all(MYSQLI_ASSOC);
-        } catch (\Throwable $th) {
-            return false;
-        }
-    }
-
-    public function actualizar($tabla, $campos, $condicion) {
-        try {
-            $resultado = $this->conexion->query("UPDATE $tabla SET $campos WHERE $condicion") or die();
-            return true;
-        } catch (\Throwable $th) {
-            return false;
-        }
-    }
-
-    public function hayRegistro($tabla, $condicion) {
-        try {
-            $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion") or die();
-            $resultado = $resultado->fetch_all(MYSQLI_ASSOC);
-            $numero = count($resultado);
-            return $numero;
-        } catch (\Throwable $th) {
-            return false;
-        }
-    }
-
-    public function eliminar($tabla, $condicion) {
-        // $resultado = $this->conexion->query("DELETE FROM $tabla WHERE $condicion") or die($this->conexion->error);
-        // if ($resultado) {
-        //     return true;
-        // }
-        // return false;
-
-        try {
-            $resultado = $this->conexion->query("DELETE FROM $tabla WHERE $condicion") or die();
-            return true;
-        } catch (\Throwable $th) {
-            return false;
-        }
-
-    }
-    // public function insertar($tabla, $datos) {
-    //     $resultado = $this->conexion->query("INSERT INTO $tabla VALUES(null, $datos)") or die($this->conexion->error);
-    //     if ($resultado) {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    public function login($usuario, $password) {
-        try {
-            $resultado = $this->conexion->query("SELECT * FROM usuarios WHERE dni = $usuario;") or die();
-            return $resultado->fetch_all(MYSQLI_ASSOC);
-        } catch (\Throwable $th) {
-            return false;
-        }
-    }
-
-
-    public function borrar($tabla, $condicion) {
-        $resultado = $this->conexion->query("DELETE FROM $tabla WHERE $condicion") or die($this->conexion->error);
-        if ($resultado) {
-            return true;
-        }
-        return false;
-    }
-
-    // public function actualizar($tabla, $campos, $condicion) {
-    //     $resultado = $this->conexion->query("UPDATE $tabla SET $campos WHERE $condicion") or die($this->conexion->error);
-    //     if ($resultado) {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
-    public function buscar($tabla, $condicion) {
-        $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion") or die($this->conexion->error);
-        if ($resultado) 
-            return $resultado->fetch_all(MYSQLI_ASSOC);
-        return false;
-    }
 }
+
+?>
--- funciones/acciones.php
+++ funciones/acciones.php
@@ -4,6 +4,9 @@
     require "../src/Exception.php";
     require "../src/PHPMailer.php";
     require "../src/SMTP.php";
+    require("../conexion/conexion.php");
+    $user = new ApptivaDB();
+
     $accion = "mostrar";
     $res = array("error" => false);
     $archivoPdf = null;
@@ -26,116 +29,130 @@
             $fecha          = $_POST["fecha"];
             $mail           = $_POST["mail"];
             $mailCopia      = $_POST["mailCopia"];
-            $pedidoTabla    = $_POST["pedido"];
-            $pedidoTabla    = explode(';', $pedidoTabla);
+            $pedido    = $_POST["pedido"];
+            $pedidoTabla    = explode(';', $pedido);
             $otros          = $_POST["otros"];
 
-            $otrosFormateado = ""; 
-            if ($otros != null) {
-                $cantidadRenglones = ceil(strlen($otros) / 95);
-                for ($i = 0; $i < $cantidadRenglones; $i++) {
-                    $inicial = 95 * $i;
-                    $final = 95;
-                    if($final > strlen($otros)){
-                        $final = strlen($otros);
-                    }
-                    $string = substr($otros, $inicial, $final) . "\n";  
-                    $otrosFormateado = $otrosFormateado . $string;
-                }
-            }
+            $pedidoBase = $pedido . ", otros : " . $otros;
+            date_default_timezone_set('America/Argentina/Cordoba');
+            $date = date("Y-m-d H:i:s");
 
+            $data = "'" . $date . "', '" . $direccionEnvio . "', '" . $ciudad . "', '" . $provincia . "', '" . $codigoPostal . "', '" . $telefono . "', '" . $pedidoBase . "', '" . 0 . "'";
+            $u = $user -> insertar("pedidos", $data);
 
-            try {
-                $pdf = new PDF();
-                $pdf->AliasNbPages();
-                $header = array('Listado de articulos pedidos');
-                $pdf->AddPage();
-                $pdf->SetFont('Arial','B',10);
-                $pdf->Cell(0,5,"Nuevo pedido de " . utf8_decode($nombreSiPueden),0,1,'C');
-                $pdf->Cell(0,10,'Fecha: ' . $fecha,0,1);
-                $pdf->Cell(0,10, utf8_decode('Dirección: ') . utf8_decode($direccionEnvio),0,1);
-                $pdf->Cell(0,10,'Ciudad: ' . utf8_decode($ciudad),0,1);
-                $pdf->Cell(0,10,'Provincia: ' . utf8_decode($provincia),0,1);
-                $pdf->Cell(0,10, utf8_decode('Código postal: ') . utf8_decode($codigoPostal),0,1);
-                $pdf->Cell(0,10, utf8_decode('Teléfono: ') . utf8_decode($telefono),0,1);
-                $pdf->SetFont('Arial','B',10);
-                $pdf->Cell(0,10,'Articulos pedidos: ' ,0,1);
-                $pdf->SetFont('Arial','',10);
-                $pdf->TablaSimple($header, $pedidoTabla);
-
-                foreach ($pedidoTabla as $key => $value) {
-                    $pdf->SetFont('Arial','',8);
-                    $pdf->Cell(0,10, utf8_decode($value) ,0,1);
-                }
-
-                $pdf->SetFont('Arial','B',10);
-                if($otrosFormateado != '' && $otrosFormateado != null) {
-                    $pdf->Cell(0,10,'Otros: ',0,1);
-                    $pdf->SetFont('Arial','',10);
-                    $pdf->Multicell(190,10,utf8_decode($otrosFormateado),1);
-                }
-               
-                
-                $archivoPdf = $pdf->Output('','S');                  
-
-                $email_user = "pedidosresidencias@hotmail.com";
-                $email_password = "pedidos.1379";
-
-                $the_subject = "Nuevo pedido de " . $nombreSiPueden;
-                $address_to = $mail;
-                $from_name = "Si Pueden";
-                $phpmailer = new PHPMailer();
-                // ———- datos de la cuenta de Gmail ——————————-
-                $phpmailer->Username = $email_user;
-                $phpmailer->Password = $email_password; 
-                
-                $phpmailer->Host = "smtp.office365.com"; // GMail
-                $phpmailer->SMTPSecure = 'STARTTLS';
-                
-                $phpmailer->Port = 587;
-                $phpmailer->IsSMTP(); // use SMTP
-                $phpmailer->SMTPAuth = true;
-                $phpmailer->setFrom($phpmailer->Username,$from_name);
-                $phpmailer->AddAddress($address_to); // recipients email
-                if ($mailCopia) {
-                    $phpmailer->AddBCC($mail);
-                    $address_to = $mailCopia;
-                }
-                $phpmailer->Subject = $the_subject;	
-
-                $phpmailer->Body .="<p>Nuevo pedido de </p>" . $nombreSiPueden . " - ";
-                $phpmailer->Body .= $ciudad . ", " . $provincia;
-                $phpmailer->Body .= "<p>Fecha: " . $fecha ."</p>";
-                $phpmailer->IsHTML(true);
-                $phpmailer->AddStringAttachment($archivoPdf, $nombreSiPueden . '.pdf','base64');
-                try {
-                    $phpmailer->smtpConnect([
-                        'ssl' => [
-                            'verify_peer' => false,
-                            'verify_peer_name' => false,
-                            'allow_self_signed' => true
-                        ]
-                    ]);
-                    $mensaje = "Pedido enviado correctamente";
-                    $res["mensaje"] = $mensaje;
-                    $res["error"] = false;
-                } catch (\Throwable $th) {
-                    $mensaje = "El pedido no se pudo enviar. Intente nuevamente";
-                    $res["mensaje"] = $mensaje;
-                    $res["error"] = true;
-                    die;
-                }
-                if(!$phpmailer->send()) { 
-                    $mensaje = "El pedido no se pudo enviar. Intente nuevamente";
-                    $res["mensaje"] = $mensaje;
-                    $res["error"] = true;
-                    die;
-                }
-            } catch (\Throwable $th) {
-                $mensaje = "Hubo un error al enviar el pedido. Intente nuevamente";
-                $res["mensaje"] = $mensaje;
+            if (!$u) { 
+                $res["mensaje"] = "El pedido no pudo realizarse";
                 $res["error"] = true;
+                return;
+            } else {
+                $otrosFormateado = ""; 
+                if ($otros != null) {
+                    $cantidadRenglones = ceil(strlen($otros) / 95);
+                    for ($i = 0; $i < $cantidadRenglones; $i++) {
+                        $inicial = 95 * $i;
+                        $final = 95;
+                        if($final > strlen($otros)){
+                            $final = strlen($otros);
+                        }
+                        $string = substr($otros, $inicial, $final) . "\n";  
+                        $otrosFormateado = $otrosFormateado . $string;
+                    }
+                }
+    
+    
+                try {
+                    $pdf = new PDF();
+                    $pdf->AliasNbPages();
+                    $header = array('Listado de articulos pedidos');
+                    $pdf->AddPage();
+                    $pdf->SetFont('Arial','B',10);
+                    $pdf->Cell(0,5,"Nuevo pedido de " . utf8_decode($nombreSiPueden),0,1,'C');
+                    $pdf->Cell(0,10,'Fecha: ' . $fecha,0,1);
+                    $pdf->Cell(0,10, utf8_decode('Dirección: ') . utf8_decode($direccionEnvio),0,1);
+                    $pdf->Cell(0,10,'Ciudad: ' . utf8_decode($ciudad),0,1);
+                    $pdf->Cell(0,10,'Provincia: ' . utf8_decode($provincia),0,1);
+                    $pdf->Cell(0,10, utf8_decode('Código postal: ') . utf8_decode($codigoPostal),0,1);
+                    $pdf->Cell(0,10, utf8_decode('Teléfono: ') . utf8_decode($telefono),0,1);
+                    $pdf->SetFont('Arial','B',10);
+                    $pdf->Cell(0,10,'Articulos pedidos: ' ,0,1);
+                    $pdf->SetFont('Arial','',10);
+                    $pdf->TablaSimple($header, $pedidoTabla);
+    
+                    foreach ($pedidoTabla as $key => $value) {
+                        $pdf->SetFont('Arial','',8);
+                        $pdf->Cell(0,10, utf8_decode($value) ,0,1);
+                    }
+    
+                    $pdf->SetFont('Arial','B',10);
+                    if($otrosFormateado != '' && $otrosFormateado != null) {
+                        $pdf->Cell(0,10,'Otros: ',0,1);
+                        $pdf->SetFont('Arial','',10);
+                        $pdf->Multicell(190,10,utf8_decode($otrosFormateado),1);
+                    }
+                   
+                    
+                    $archivoPdf = $pdf->Output('','S');                  
+    
+                    $email_user = "pedidosresidencias@hotmail.com";
+                    $email_password = "pedidos.1379";
+    
+                    $the_subject = "Nuevo pedido de " . $nombreSiPueden;
+                    $address_to = $mail;
+                    $from_name = "Si Pueden";
+                    $phpmailer = new PHPMailer();
+                    // ———- datos de la cuenta de Gmail ——————————-
+                    $phpmailer->Username = $email_user;
+                    $phpmailer->Password = $email_password; 
+                    
+                    $phpmailer->Host = "smtp.office365.com"; // GMail
+                    $phpmailer->SMTPSecure = 'STARTTLS';
+                    
+                    $phpmailer->Port = 587;
+                    $phpmailer->IsSMTP(); // use SMTP
+                    $phpmailer->SMTPAuth = true;
+                    $phpmailer->setFrom($phpmailer->Username,$from_name);
+                    $phpmailer->AddAddress($address_to); // recipients email
+                    if ($mailCopia) {
+                        $phpmailer->AddBCC($mail);
+                        $address_to = $mailCopia;
+                    }
+                    $phpmailer->Subject = $the_subject;	
+    
+                    $phpmailer->Body .="<p>Nuevo pedido de </p>" . $nombreSiPueden . " - ";
+                    $phpmailer->Body .= $ciudad . ", " . $provincia;
+                    $phpmailer->Body .= "<p>Fecha: " . $fecha ."</p>";
+                    $phpmailer->IsHTML(true);
+                    $phpmailer->AddStringAttachment($archivoPdf, $nombreSiPueden . '.pdf','base64');
+                    try {
+                        $phpmailer->smtpConnect([
+                            'ssl' => [
+                                'verify_peer' => false,
+                                'verify_peer_name' => false,
+                                'allow_self_signed' => true
+                            ]
+                        ]);
+                        $mensaje = "Pedido enviado correctamente";
+                        $res["mensaje"] = $mensaje;
+                        $res["error"] = false;
+                    } catch (\Throwable $th) {
+                        $mensaje = "El pedido no se pudo enviar. Intente nuevamente";
+                        $res["mensaje"] = $mensaje;
+                        $res["error"] = true;
+                        die;
+                    }
+                    if(!$phpmailer->send()) { 
+                        $mensaje = "El pedido no se pudo enviar. Intente nuevamente";
+                        $res["mensaje"] = $mensaje;
+                        $res["error"] = true;
+                        die;
+                    }
+                } catch (\Throwable $th) {
+                    $mensaje = "Hubo un error al enviar el pedido. Intente nuevamente";
+                    $res["mensaje"] = $mensaje;
+                    $res["error"] = true;
+                }
             }
+
         break;
 
         default:
