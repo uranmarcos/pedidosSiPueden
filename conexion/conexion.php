16d810785e788f9fdc5b04a5b88065a34db55a29
uranmarcos <marcos_uran@hotmail.com>
2023-03-12 19:02:28 -0300

    limpieza de codigo

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -5,17 +5,6 @@ class ApptivaDB {
     private $clave = "";
     private $db = "pedidossipueden";
     public $conexion;
-
-    // private $host = "localhost";
-    // private $usuario = "fundaci_pedidos";
-    // private $clave = "pedidos.1379";
-    // private $db = "fundaci_pedidos";
-    // public $conexion;
-
-
-    // $dsn = "mysql:dbname=fundaci_pedidos; host=localhost; port=21";
-    // $usuario = "fundaci_pedidos";
-    // $pass = "pedidos.1379";
     
     public function __construct(){
         $this->conexion = new mysqli($this->host, $this->usuario, $this->clave, $this->db)
